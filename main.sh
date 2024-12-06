e56610569cfa44038684814560b017aac35c97ee
d3fectlulz <jordanyates@beekmancharter.org>
2024-12-06 14:46:20 -0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,10 +2,10 @@
 while :
 do
 echo -e "\e[1;37m-------------------------------------------\e[0m"
-echo -n -e "\e[1;32mDesea regresar al menu\e[0m \e[1;37m[\e[0m\e[1;31msi\e[0m\e[1;37m]\e[0m \e[1;32msalir\e[0m \e[1;37m[\e[0m\e[1;31mno\e[1;37m]\e[0m \e[1;32m>\e[0m \e[1;37m\e[0m"
-read opcion
-case $opcion in
-si)
+echo -n -e "\e[1;32mDo you want to return to the menu?\e[0m \e[1;37m[\e[0m\e[1;31msi\e[0m\e[1;37m]\e[0m \e[1;32msalir\e[0m \e[1;37m[\e[0m\e[1;31mno\e[1;37m]\e[0m \e[1;32m>\e[0m \e[1;37m\e[0m"
+read option
+case $option in
+yes)
 cd /data/data/com.termux/files/home/IP-OSINT
 #! /bin/bash
 bash osint.sh
