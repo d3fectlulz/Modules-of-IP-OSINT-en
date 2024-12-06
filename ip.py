66caa7e9356da41a2cd412a10aa406fa09abf6c3
d3fectlulz <jordanyates@beekmancharter.org>
2024-12-06 14:45:38 -0600

    Update ip.py

--- ip.py
+++ ip.py
@@ -1,9 +1,9 @@
 from socket import gethostbyname
 from os import system
 
-print("\033[1;33m*[Bienvenido a IP-OSINT: ]*")
+print("\033[1;33m*[Welcome to IP-OSINT! ]*")
 print("")
-target = input("\033[1;32mIngrese el host: \033[1;37m")
+target = input("\033[1;32mEnter the host: \033[1;37m")
 targetIP = gethostbyname(target)
 print(f"\033[1;31m-Target IP ===>', \033[1;37m{targetIP}")
 print("")
