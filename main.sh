ed8f932f809b43ecb180b12d958ce644e82e9b0c
d3fectlulz <jordanyates@beekmancharter.org>
2024-12-29 19:58:55 -0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 while :
 do
 echo -e "\e[1;37m-------------------------------------------\e[0m"
-echo -n -e "\e[1;32mDo you want to return to the menu?\e[0m \e[1;37m[\e[0m\e[1;31msi\e[0m\e[1;37m]\e[0m \e[1;32msalir\e[0m \e[1;37m[\e[0m\e[1;31mno\e[1;37m]\e[0m \e[1;32m>\e[0m \e[1;37m\e[0m"
+echo -n -e "\e[1;32mDo you want to return to the menu?\e[0m \e[1;37m[\e[0m\e[1;31myes\e[0m\e[1;37m]\e[0m \e[1;32mexit\e[0m \e[1;37m[\e[0m\e[1;31mno\e[1;37m]\e[0m \e[1;32m>\e[0m \e[1;37m\e[0m"
 read option
 case $option in
 yes)
